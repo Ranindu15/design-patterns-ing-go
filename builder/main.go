fb2b3cdfae1d8120c74322d792f5ab17ac9565cc
ranindu <raninduj@pickme.lk>
2020-12-30 18:47:48 +0530

    Update builder

--- builder/main.go
+++ builder/main.go
@@ -31,3 +31,26 @@ func getBuilder(builderType string) iBuilder {
 	}
 	return nil
 }
+func newNormalBuilder() *normalBuilder {
+	return &normalBuilder{}
+}
+
+func (b *normalBuilder) setWindowType() {
+	b.windowType = "Wooden Window"
+}
+
+func (b *normalBuilder) setDoorType() {
+	b.doorType = "Wooden Door"
+}
+
+func (b *normalBuilder) setNumFloor() {
+	b.floor = 2
+}
+
+func (b *normalBuilder) getHouse() house {
+	return house{
+		doorType:   b.doorType,
+		windowType: b.windowType,
+		floor:      b.floor,
+	}
+}
